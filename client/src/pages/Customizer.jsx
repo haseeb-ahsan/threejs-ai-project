71e128f3fba47205ee420a6a34527b62e9aeed1b
Haseeb Ahsan Tahir <84566594+haseeb-ahsan@users.noreply.github.com>
2024-11-24 23:03:23 +0000

    Update Customizer.jsx

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -59,7 +59,7 @@ const Customizer = () => {
     try {
       // call our backend to generate an ai image
       setGeneratingImg(true);
-      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+      const response = await fetch('https://threejs-ai-project-3oie.onrender.com/api/v1/dalle', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
